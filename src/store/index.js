a61f0105432142bebba65da329224637286270bf
Sumit Bhade <imsumitbhade@gmail.com>
2025-01-27 00:41:01 +0530

    Migrating to Redux Toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,8 @@
-import { createStore } from "redux";
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-createSlice({
+const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
@@ -14,10 +13,10 @@ createSlice({
       state.counter--;
     },
     increase(state, action) {
-      state.counter = state.counter + action.amount;
+      state.counter = state.counter + action.payload;
     },
     toggle(state) {
-      state.showCounter = !showCounter;
+      state.showCounter = !state.showCounter;
     },
   },
 });
@@ -25,5 +24,7 @@ createSlice({
 const store = configureStore({
   reducer: counterSlice.reducer,
 });
+
+export const counterActions = counterSlice.actions;
 
 export default store;
